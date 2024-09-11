f4271472f987b872ddeb4e96f69c7e0d90d5c083
Catalin <20538711+devcatalin@users.noreply.github.com>
2024-09-11 15:26:55 +0300

    fix test

--- junit5-allure/src/test/java/io/github/bonigarcia/PlayingWithAllureTest.java
+++ junit5-allure/src/test/java/io/github/bonigarcia/PlayingWithAllureTest.java
@@ -18,6 +18,7 @@ package io.github.bonigarcia;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTruet;
 
 class PlayingWithAllureTest {
 
